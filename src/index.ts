5aafbf3c194b3071287648d60286dc932cd84ea6
Arthur <catce.2021211mtds0470@aluno.ifpi.edu.br>
2022-11-10 17:17:03 -0300

    src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from 'express'
+import { uuid } from 'uuidv4';
+
+interface User {
+    nome: string;
+    tel: string;
+    idade: number;
+}
+
+const listaUsuarios: User[] = []
+
+const server = express()
+server.use(express.json())
+
+server.get('/api/v1/users', (req: Request, res: Response) => {
+    return res.json(listaUsuarios)
+})
+
+server.post('/api/v1/users', (req: Request, res: Response) => {
+    const {nome, tel, idade} = req.body
+
+    const newUser: User = {
+        nome: nome,
+        tel: tel,
+        idade: idade
+    }
+    
+    listaUsuarios.push(newUser)
+
+    return res.json(newUser)
+})
+
+server.listen(3000, () => {
+    console.log('Servidor iniciado na porta 3000...')
+})
